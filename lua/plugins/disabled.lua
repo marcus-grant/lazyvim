49b50f0e395a1ce3c6f6bf994158ce0faf635388
Marcus Grant <marcusfg@gmail.com>
2024-06-05 14:12:32 +0200

    Add a disable lazy/lua/plugins cfg

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,11 @@
+-- Disable plugins here
+-- Doc page in question here:
+-- https://www.lazyvim.org/configuration/plugins/#-disabling-plugins
+return {
+  -- disable default bufferline
+  { "akinsho/bufferline.nvim", enabled = false },
+  -- {
+  --   "nvim-bufferline.lua",
+  --   enabled = false,
+  -- },
+}
