8284d378bbbd4b24e1122092a5f11efbeed3b5be
Marcus Grant <marcusfg@gmail.com>
2024-12-17 16:46:12 +0100

    Disable malfunctioning indentline plugin

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -4,6 +4,8 @@
 return {
   -- disable default bufferline
   { "akinsho/bufferline.nvim", enabled = false },
+  { "lukas-reineke/headlines.nvim", enabled = false },
+  { "lukas-reineke/indent-line.nvim", enabled = false },
   -- {
   --   "nvim-bufferline.lua",
   --   enabled = false,
